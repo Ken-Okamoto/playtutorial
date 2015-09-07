94a3fac003128051fc10a7e0665e40e7ca223952
Ken-Okamoto <ken.okamoto@opentone.co.jp>
2015-09-07 16:25:35 +0900

    Personに血液型と好きな数字を追加

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -1,5 +1,5 @@
 package models
 
-case class Person(age:Int, name:Name)
+case class Person(age:Int, name:Name, bloodType:Option[String], favoriteNumber:Seq[Int])
 //case class Name(first:String, last:String)
 case class Name(first:String, middle:Option[String], last:String)
